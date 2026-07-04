f1ed93742207b1f83bae09fa6406072fedc3b297
Miriam Alvarez <map.miriam96@gmail.com>
2026-07-03 21:45:10 -0700

    day 1

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,7 +1,13 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
+import sys
+print(sys.version)
 
 print("Hello World!")   # print hello world
+print(['Mir', 'Python', 'Mexico'])   # List
+print({'name': 'Mir', 'country': 'Mexico', 'city': 'Mexico City'})   # Dictionary
+print("I am enjoying 30 days of python")
+
 
 print(2 + 3)   # addition(+)
 print(3 - 1)   # subtraction(-)
@@ -18,8 +24,22 @@ print(3 // 2)  # Floor division operator(//)
 
 print(type(10))                  # Int
 print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type(1 + 4j))              # Complex
+print(type('Mir'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type({'name': 'Mir'}))  # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
+# Dictionary: 
+i = {'goat':7,'dog':8,'elephant':9}
+print(type(i))
+
+
+# Find an Euclidean distance between (2, 3) and (10, 8)
+
+p1=[2,3]
+p2=[10,8]
+distance = ((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)**0.5
+print("Euclidean distance between", p1, "and", p2, "is:", distance)
+
+import math
+print(math.dist([2,3],[10,8]))
