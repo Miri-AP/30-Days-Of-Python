949059c653a48da9777ef861ff779c45cf43deeb
Miriam Alvarez <map.miriam96@gmail.com>
2026-07-08 20:09:27 -0700

    day 6: level 2

--- 06_Day_Tuples/exercises.py
+++ 06_Day_Tuples/exercises.py
@@ -15,3 +15,39 @@ print('Number of siblings:', num_siblings)
 # 5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
 family_members = siblings + ('Esme', 'Carlisle')
 print('Family members:', family_members)
+
+## Level 2
+# 1. Unpack siblings and parents from family_members
+sisters = family_members[:3]
+brothers = family_members[3:6]
+parents = family_members[6:]
+print('Sisters:', sisters)
+print('Brothers:', brothers)
+print('Parents:', parents)
+# 2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+fruits = ('apple', 'banana', 'orange')
+vegetables = ('carrot', 'broccoli', 'spinach')
+animal_products = ('milk', 'cheese', 'eggs')
+food_stuff_tp = fruits + vegetables + animal_products
+print('Food stuff tuple:', food_stuff_tp)
+# 3. Change the about food_stuff_tp tuple to a food_stuff_lt list
+food_stuff_lt = list(food_stuff_tp)
+print('Food stuff list:', food_stuff_lt)
+# 4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+middle_items = food_stuff_lt[len(food_stuff_lt)//2]
+print('Middle item(s):', middle_items)
+# 5. Slice out the first three items and the last three items from food_stuff_lt list
+first_three = food_stuff_lt[:3]
+last_three = food_stuff_lt[-3:]
+print('First three items:', first_three)
+print('Last three items:', last_three)
+# 6. Delete the food_stuff_tp tuple completely
+del food_stuff_tp
+# 7. Check if an item exists in tuple:
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+# Check if 'Estonia' is a nordic country
+print('Estonia' in nordic_countries)
+
+#Check if 'Iceland' is a nordic country
+print('Iceland' in nordic_countries)
